30351c03281fcc3e8d2c088dea142da1021f93d5
Michel-CMG <64927362+Michel-CMG@users.noreply.github.com>
2020-05-30 16:05:41 +0900

    Client send the request to TCP Socket and receive the response. Print in console. Have not test in Program yet.

--- Client.cs
+++ Client.cs
@@ -12,17 +12,61 @@ namespace tcpip_ex01_dotnetcore
         }
         public Message Send(Message requestMsg)
         {
-            // Do nothing but print the request and response Message
-            Console.WriteLine($"Client says: send request '{requestMsg}'");
-
-            var responseMsg = new Message
+            using (var client = new System.Net.Sockets.TcpClient())
             {
-                id = requestMsg.id,
-                content = requestMsg.content
-            };
+                // 01 Connect to server
+                try
+                {
+                    client.Connect(this.endpoint);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Client says: Server is not running. Quit.");
+                    var errorMessage = new Message(99, "Server is not running.");
+                    return errorMessage;
+                }
 
-            Console.WriteLine($"Client says: receive response '{responseMsg}'");
-            return responseMsg;
+                // 02 Send request message
+                var utf8 = System.Text.Encoding.UTF8;
+                using (var stream = client.GetStream())
+                using (var bWriter = new System.IO.BinaryWriter(stream, utf8))
+                {
+                    var requestBytes = this.MessageToBytes(requestMsg);
+                    bWriter.Write(requestBytes.Length);
+                    bWriter.Write(requestBytes);
+                }
+                Console.WriteLine($"Client says: send request '{requestMsg}'");
+
+                // 03 Get response message
+                using (var stream = client.GetStream())
+                using (var bReader = new System.IO.BinaryReader(stream, utf8))
+                {
+                    var length = bReader.ReadInt32();
+                    var bytes = bReader.ReadBytes(length);
+                    var responseMsg = this.BytesToMessage(bytes);
+                    Console.WriteLine($"Client says: receive response '{responseMsg}'");
+                    return responseMsg;
+                }
+            }
+        }
+
+        private Byte[] MessageToBytes(Message message)
+        {
+            var formatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            using (var stream = new System.IO.MemoryStream())
+            {
+                formatter.Serialize(stream, message);
+                return stream.ToArray();
+            }
+        }
+
+        private Message BytesToMessage(Byte[] bytes)
+        {
+            var formatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            using (var stream = new System.IO.MemoryStream(bytes))
+            {
+                return (Message)formatter.Deserialize(stream);
+            }
         }
     }
 }
