92771c8b3a00bcf6427cf66fb781fb93fcbc4064
Michel-CMG <64927362+Michel-CMG@users.noreply.github.com>
2020-05-30 14:37:06 +0900

    Select 0 or any other to run server or run client. But do nothing now.

--- Program.cs
+++ Program.cs
@@ -6,24 +6,52 @@ namespace tcpip_ex01_dotnetcore
     {
         static void Main(string[] args)
         {
+            PrintHello();
+
+            Console.WriteLine("\n\n\n");
+            Console.WriteLine("Select run the server or run the client");
+            Console.WriteLine("0: Run the server");
+            Console.WriteLine("Any other: Run the client");
+            Console.WriteLine();
+
+            string command = Console.ReadLine();
+            switch(command)
+            {
+                case "0":
+                    StartServer();
+                    break;
+                default :
+                    StartClient();
+                    break;
+            }
+        }
+
+        private static void PrintHello()
+        {
             var hello = new Message(10, "Hello");
             var world = new Message(20, "World with class Message");
 
             Console.WriteLine($"{hello.content} {world.content}!");
-
             Console.Write(hello);
             Console.Write(world);
             Console.WriteLine();
+        }
 
+        private static void StartServer()
+        {
             var server = new Server();
-            var client = new Client();
             Console.WriteLine($"The server running status: {server.IsStart()}");
-
-            client.Send(hello);
-            client.Send(world);
             // Wait 2 seconds because server is do nothing now
             System.Threading.Thread.Sleep(2000);
             server.Stop();
+        }
+
+        private static void StartClient()
+        {
+            var client = new Client();
+            client.Send(new Message(10, "あいうえお"));
+            client.Send(new Message(20, "かきくけこ"));
+            client.Send(new Message(00, "stop the server"));
         }
     }
 }
