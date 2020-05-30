646b9d699e1be1e514e91d4e799fd5bed735669d
Michel-CMG <64927362+Michel-CMG@users.noreply.github.com>
2020-05-30 19:23:45 +0900

    Message: implement ToBytes and BytesToMessage.

--- Message.cs
+++ Message.cs
@@ -1,3 +1,5 @@
+using System.Runtime.Serialization.Formatters.Binary;
+
 namespace tcpip_ex01_dotnetcore
 {
     [System.Serializable]
@@ -18,6 +20,25 @@ namespace tcpip_ex01_dotnetcore
         {
             string idToPrint = this.id.ToString("D2");
             return $"{{ id:{idToPrint}, content:\"{this.content}\" }}";
+        }
+
+        public byte[] ToBytes()
+        {
+            var formatter = new BinaryFormatter();
+            using (var stream = new System.IO.MemoryStream())
+            {
+                formatter.Serialize(stream, this);
+                return stream.ToArray();
+            }
+        }
+
+        public static Message BytesToMessage(byte[] bytes)
+        {
+            var formatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            using (var stream = new System.IO.MemoryStream(bytes))
+            {
+                return (Message)formatter.Deserialize(stream);
+            }
         }
     }
 }
